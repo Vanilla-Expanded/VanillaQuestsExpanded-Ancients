add510887a48dcfb1db2b015cc0362a3059fbd0a
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-09-15 20:37:56 +0200

    Genes

---
+++ 1.6/Source/DefOfs/InternalDefOf.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+namespace VanillaQuestsExpandedAncients
+{
+	[DefOf]
+	public static class InternalDefOf
+	{
+		static InternalDefOf()
+		{
+			DefOfHelper.EnsureInitializedInCtor(typeof(InternalDefOf));
+		}
+
+		public static ThingDef VQEA_PawnLevitator;
+		public static GeneDef VQEA_Serene;
+        public static GeneDef VQEA_SubstanceImpervious;
+
+    }
+}
---
+++ 1.6/Source/Harmony/CompDrug_PostIngested.cs
@@ -0,0 +1,57 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using Verse;
+
+namespace VanillaQuestsExpandedAncients
+{
+
+    [HarmonyPatch(typeof(CompDrug), "PostIngested")]
+    public static class VanillaQuestsExpandedAncients_CompDrug_PostIngested_Patch
+    {
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var codes = codeInstructions.ToList();
+
+            var genedef = AccessTools.Field(typeof(Gene), "def");
+            var detectNoOverdoseGene = AccessTools.Method(typeof(VanillaQuestsExpandedAncients_CompDrug_PostIngested_Patch), "DetectNoOverdoseGene");
+           
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+               
+                if ( codes[i].opcode == OpCodes.Stloc_2)
+                {
+                    yield return codes[i];
+                  
+                   
+                    yield return new CodeInstruction(OpCodes.Ldloc_0);
+                    yield return new CodeInstruction(OpCodes.Ldloc_2);
+                    yield return new CodeInstruction(OpCodes.Ldfld, genedef);
+                    yield return new CodeInstruction(OpCodes.Call, detectNoOverdoseGene);
+                    yield return new CodeInstruction(OpCodes.Stloc_0);
+
+                }
+
+
+                else yield return codes[i];
+            }
+        }
+
+
+        public static float DetectNoOverdoseGene(GeneDef gene)
+        {
+            if(gene == InternalDefOf.VQEA_SubstanceImpervious)
+            {
+                return 0;
+
+            }else return 1;
+        }
+
+    }
+}
