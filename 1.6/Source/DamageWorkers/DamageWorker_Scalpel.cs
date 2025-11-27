b1665df21ec98ac32ba00c82cee24a3f28e84fd3
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-11-27 19:54:55 +0100

    Scalpel NOT working

---
+++ 1.6/Source/DamageWorkers/DamageWorker_Scalpel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace VanillaQuestsExpandedAncients
+{
+
+    public class DamageWorker_Scalpel : DamageWorker_Cut
+    {
+
+
+        protected override void ApplySpecialEffectsToPart(Pawn pawn, float totalDamage, DamageInfo dinfo, DamageWorker.DamageResult result)
+        {
+            base.ApplySpecialEffectsToPart(pawn, totalDamage, dinfo, result);
+
+            Pawn attacker = dinfo.Instigator as Pawn;
+            if (attacker != null)
+            {
+                float extraDamage = attacker.skills.GetSkill(SkillDefOf.Medicine).Level;
+                pawn.TakeDamage(new DamageInfo(DamageDefOf.Cut, extraDamage, 0f, -1f, attacker, null, dinfo.Weapon, DamageInfo.SourceCategory.ThingOrUnknown));
+
+            }
+
+
+
+        }
+    }
+}
